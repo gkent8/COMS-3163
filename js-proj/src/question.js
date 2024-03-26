0e82c26d320772cda82bb9b79134df9a77cba912
Gavin Kent <gkent@atu.edu>
2024-03-26 17:31:52 -0500

    quiz app

---
+++ js-proj/src/question.js
@@ -0,0 +1,14 @@
+const questionContent = [
+  {
+    type: 'trueFalse',
+    question: 'string',
+    answer: 'boolean',
+    correctAnswer: 'boolean',
+  },
+  {
+    type: 'multipleChoice',
+    question: 'string',
+    answer: 'string[]',
+    correntAnswer: 'number[]',
+  },
+];
